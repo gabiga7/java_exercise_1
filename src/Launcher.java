8ea1f60882fb2cffa412d94e5c6ab168ab80cd6a
Gabriel QUINT <gabriel.quint@epita.fr>
2022-02-10 10:46:58 +0100

    loop

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenue");
-        Scanner scanner = new Scanner(System.in);
-        String s= scanner.next();
-        if (s.contentEquals("quit"))
-            System.exit(1);
-        else
-            System.out.println("Unknown command");
+        while (1==1)
+        {
+            Scanner scanner = new Scanner(System.in);
+            String s = scanner.next();
+            if (s.contentEquals("quit"))
+                System.exit(1);
+            else
+                System.out.println("Unknown command");
+        }
     }
 }
